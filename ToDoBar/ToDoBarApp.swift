e1cbcbf6bf8c881095f1249ac894645c3d6854e5
bilet-13 <r10922114@csie.ntu.edu.tw>
2025-04-13 21:04:56 +0800

    Initial Commit

---
+++ ToDoBar/ToDoBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoBarApp.swift
+//  ToDoBar
+//
+//  Created by william on 2025/4/13.
+//
+
+import SwiftUI
+
+@main
+struct ToDoBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoBarTests/ToDoBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoBarTests.swift
+//  ToDoBarTests
+//
+//  Created by william on 2025/4/13.
+//
+
+import Testing
+@testable import ToDoBar
+
+struct ToDoBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
